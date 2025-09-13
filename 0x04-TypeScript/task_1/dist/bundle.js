b91296f3a4674ce551bea4d2886396f58d8de4a7
bfcastro <baffourcastro@gmail.com>
2025-09-13 03:08:31 +0000

    0x04-TypeScript

---
+++ 0x04-TypeScript/task_1/dist/bundle.js
@@ -0,0 +1,17 @@
+/******/ (() => { // webpackBootstrap
+/*!********************!*\
+  !*** ./js/main.ts ***!
+  \********************/
+//usage example
+var teacher3 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: false,
+    location: 'London',
+    contract: false,
+};
+console.log(teacher3);
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7QUFTQSxlQUFlO0FBQ2YsSUFBTSxRQUFRLEdBQVk7SUFDMUIsU0FBUyxFQUFFLE1BQU07SUFDakIsUUFBUSxFQUFFLEtBQUs7SUFDZixnQkFBZ0IsRUFBRSxLQUFLO0lBQ3ZCLFFBQVEsRUFBRSxRQUFRO0lBQ2xCLFFBQVEsRUFBRSxLQUFLO0NBQ2QsQ0FBQztBQUVGLE9BQU8sQ0FBQyxHQUFHLENBQUMsUUFBUSxDQUFDLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90eXBlc2NyaXB0X2RlcGVuZGVuY2llcy8uL2pzL21haW4udHMiXSwic291cmNlc0NvbnRlbnQiOlsiaW50ZXJmYWNlIFRlYWNoZXJ7XG5yZWFkb25seSBmaXJzdE5hbWU6IHN0cmluZztcbnJlYWRvbmx5IGxhc3ROYW1lOiBzdHJpbmc7XG5mdWxsVGltZUVtcGxveWVlOiBib29sZWFuO1xueWVhcnNPZkV4cGVyaWVuY2U/OiBudW1iZXI7XG5sb2NhdGlvbjogc3RyaW5nO1xuW3Byb3BOYW1lOiBzdHJpbmddOiBhbnk7XG59XG5cbi8vdXNhZ2UgZXhhbXBsZVxuY29uc3QgdGVhY2hlcjM6IFRlYWNoZXIgPSB7XG5maXJzdE5hbWU6ICdKb2huJyxcbmxhc3ROYW1lOiAnRG9lJyxcbmZ1bGxUaW1lRW1wbG95ZWU6IGZhbHNlLFxubG9jYXRpb246ICdMb25kb24nLFxuY29udHJhY3Q6IGZhbHNlLFxufTtcblxuY29uc29sZS5sb2codGVhY2hlcjMpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- 0x04-TypeScript/task_1/webpack.config.js
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -1,37 +1,30 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./js/main.ts",
   devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'ts-loader',
-        options: {
-          transpileOnly: true
-        }
-      }
-    ]
+        loader: "ts-loader",
+        options: { transpileOnly: true }, 
+        exclude: /node_modules/,
+      },
+    ],
   },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
-  devServer: {
-    contentBase: "./dist"
-  },
-  plugins: [
-    new ForkTsCheckerWebpackPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "Development"
-    })
-  ],
+  resolve: { extensions: [".ts", ".tsx", ".js"] },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new ForkTsCheckerWebpackPlugin(), 
+  ],
 };
