b18e19ecf8ab0f1817e3680eb325686eb80389b8
bfcastro <baffourcastro@gmail.com>
2025-09-13 23:47:54 +0000

    main

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,16 @@
+// isDirector function
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+// Execute Work
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  }
+  return employee.workTeacherTasks();
+  }
+
+//Example
+console.log(executeWork(createEmployee(200))); //Getting to work
+console.log(executeWork(createEmployee(1000))); //Getting to director tasks
